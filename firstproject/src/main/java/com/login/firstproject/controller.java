3b5f528382520f19341c78f3aa65fd032a63275d
Anushka Dutta <anushka.barasat@gmail.com>
2025-10-11 15:56:21 +0530

    udated backend

--- firstproject/src/main/java/com/login/firstproject/controller.java
+++ firstproject/src/main/java/com/login/firstproject/controller.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin("*")
 @RequestMapping("/users")
 public class controller {
 

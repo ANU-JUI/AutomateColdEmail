d8d6b34bdc2094c4af63133b6d2ace94a258bab0
Anushka Dutta <anushka.barasat@gmail.com>
2025-10-11 23:00:45 +0530

    new

--- firstproject/src/main/java/com/login/firstproject/Emailconfig.java
+++ firstproject/src/main/java/com/login/firstproject/Emailconfig.java
@@ -14,7 +14,7 @@ public class Emailconfig {
     @Value("${spring.mail.host:smtp.gmail.com}")
     private String host;
 
-    @Value("${spring.mail.port:587}")
+    @Value("${spring.mail.port:465}") // Changed from 587 to 465
     private int port;
 
     @Value("${spring.mail.username:}")
@@ -34,8 +34,11 @@ public class Emailconfig {
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.ssl.enable", "true"); // Enable SSL
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
+        // Remove STARTTLS properties since we're using SSL
         props.put("mail.debug", "true");
 
         return mailSender;
